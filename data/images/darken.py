065395ef1e25cf1127a6cbc4dfd1bf15137520e4
Sivecano <Sivecano@gmail.com>
2023-11-05 13:12:03 +0100

    update helper script and standardize png

--- data/images/darken.py
+++ data/images/darken.py
@@ -2,10 +2,12 @@
 import os
 
 pairs = [(f, f.replace(".svg", "_dark.svg")) 
-         for f in os.listdir(".") if f.endswith(".svg")]
+         for f in os.listdir(".") if f.endswith(".svg") and not f.endswith("_dark.svg")]
 
 for (l, d) in pairs:
     with open(l, "r") as fl, open(d,"w") as fd:
-        fd.write(fl.read().replace("#000000", "#a0a0a0"))
-    os.system(f"inkscape {d} -o {d.replace('.svg', '.png')}")
+        fd.write(fl.read().replace("#000000", "#b4b4b4"))
+
+for f in (j for i in pairs for j in i):
+    os.system(f"inkscape {f} -o {f.replace('.svg', '.png')}")
 
